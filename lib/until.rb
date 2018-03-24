d461b798d6bfd84e00a7ba013debb839d3ef6cdb
Michael Roth <mick2090@gmail.com>
2018-03-24 14:45:20 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,9 @@
 def using_until
-levitation_force = 6
-while num_of_hotdogs_eaten < 7
-num_of_hotdogs_eaten += 1
-puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
-end
+  levitation_force = 6
+  while num_of_hotdogs_eaten < 7
+    num_of_hotdogs_eaten += 1
+    puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+  end
     
 end
 
