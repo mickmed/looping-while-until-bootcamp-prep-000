84537a328fe8455cc7c327336b41ce5063e857d8
Michael Roth <mick2090@gmail.com>
2018-03-24 14:46:33 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -3,7 +3,7 @@ def using_until
   
   while levitation_force < 10
     levitation_force += 1
-    puts "You have now eaten #{num_of_hotdogs_eaten} hot dog(s)."
+    puts "Wingardium Leviosa"
   end
     
 end
